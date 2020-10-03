c72b5a160f6af840e0149a782c2619cc0aa4effe
bota.lb <bota.lb@alibaba-inc.com>
2020-10-03 17:47:07 +0800

    suc

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const config = require('./config')
 const routes = require('./routes');
 const parser = require('./lib/parser')
 const render = require('./lib/render')
+const static =require('./lib/static')
 const app = new Koa();
 parser(app)
 //render必须让在routes之前
 render(app)
 routes(app)
+static(app)
 app.listen(config.port,()=>{
     console.log(`服务启动，打开网页：http://localhost:${config.port}`)
 });
